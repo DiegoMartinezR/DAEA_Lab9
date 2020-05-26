16255b74d5a879a45e263e44c8411ffd537eebdb
Diego Martinez Rayme <diego.martinez.r@tecsup.edu.pe>
2020-05-25 20:17:27 -0500

    lab9

---
+++ Database1/bin/Debug/Database1_6.publish.sql
@@ -0,0 +1,88 @@
+﻿/*
+Script de implementación para University
+
+Una herramienta generó este código.
+Los cambios realizados en este archivo podrían generar un comportamiento incorrecto y se perderán si
+se vuelve a generar el código.
+*/
+
+GO
+SET ANSI_NULLS, ANSI_PADDING, ANSI_WARNINGS, ARITHABORT, CONCAT_NULL_YIELDS_NULL, QUOTED_IDENTIFIER ON;
+
+SET NUMERIC_ROUNDABORT OFF;
+
+
+GO
+:setvar DatabaseName "University"
+:setvar DefaultFilePrefix "University"
+:setvar DefaultDataPath "C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\"
+:setvar DefaultLogPath "C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\"
+
+GO
+:on error exit
+GO
+/*
+Detectar el modo SQLCMD y deshabilitar la ejecución del script si no se admite el modo SQLCMD.
+Para volver a habilitar el script después de habilitar el modo SQLCMD, ejecute lo siguiente:
+SET NOEXEC OFF; 
+*/
+:setvar __IsSqlCmdEnabled "True"
+GO
+IF N'$(__IsSqlCmdEnabled)' NOT LIKE N'True'
+    BEGIN
+        PRINT N'El modo SQLCMD debe estar habilitado para ejecutar correctamente este script.';
+        SET NOEXEC ON;
+    END
+
+
+GO
+USE [$(DatabaseName)];
+
+
+GO
+
+MERGE INTO Course AS Target 
+USING (VALUES 
+        (1, 'Economics', 3), 
+        (2, 'Literature', 3), 
+        (3, 'Chemistry', 4)
+) 
+AS Source (CourseID, Title, Credits) 
+ON Target.CourseID = Source.CourseID 
+WHEN NOT MATCHED BY TARGET THEN 
+INSERT (Title, Credits) 
+VALUES (Title, Credits);
+
+MERGE INTO Student AS Target
+USING (VALUES 
+        (1, 'Tibbetts', 'Donnie', '2013-09-01'), 
+        (2, 'Guzman', 'Liza', '2012-01-12'), 
+        (3, 'Catlett', 'Phil', '2011-09-03')
+)
+AS Source (StudentID, LastName, FirstName, EnrollmentDate)
+ON Target.StudentID = Source.StudentID
+WHEN NOT MATCHED BY TARGET THEN
+INSERT (LastName, FirstName, EnrollmentDate)
+VALUES (LastName, FirstName, EnrollmentDate);
+
+MERGE INTO Enrollment AS Target
+USING (VALUES 
+(1, 2.00, 1, 1),
+(2, 3.50, 1, 2),
+(3, 4.00, 2, 3),
+(4, 1.80, 2, 1),
+(5, 3.20, 3, 1),
+(6, 4.00, 3, 2)
+)
+AS Source (EnrollmentID, Grade, CourseID, StudentID)
+ON Target.EnrollmentID = Source.EnrollmentID
+WHEN NOT MATCHED BY TARGET THEN
+INSERT (Grade, CourseID, StudentID)
+VALUES (Grade, CourseID, StudentID);
+GO
+
+GO
+PRINT N'Actualización completada.';
+
+
+GO
---
+++ Database1/bin/Debug/Database1_8.publish.sql
@@ -0,0 +1,66 @@
+﻿/*
+Script de implementación para University
+
+Una herramienta generó este código.
+Los cambios realizados en este archivo podrían generar un comportamiento incorrecto y se perderán si
+se vuelve a generar el código.
+*/
+
+GO
+SET ANSI_NULLS, ANSI_PADDING, ANSI_WARNINGS, ARITHABORT, CONCAT_NULL_YIELDS_NULL, QUOTED_IDENTIFIER ON;
+
+SET NUMERIC_ROUNDABORT OFF;
+
+
+GO
+:setvar DatabaseName "University"
+:setvar DefaultFilePrefix "University"
+:setvar DefaultDataPath "C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\"
+:setvar DefaultLogPath "C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\"
+
+GO
+:on error exit
+GO
+/*
+Detectar el modo SQLCMD y deshabilitar la ejecución del script si no se admite el modo SQLCMD.
+Para volver a habilitar el script después de habilitar el modo SQLCMD, ejecute lo siguiente:
+SET NOEXEC OFF; 
+*/
+:setvar __IsSqlCmdEnabled "True"
+GO
+IF N'$(__IsSqlCmdEnabled)' NOT LIKE N'True'
+    BEGIN
+        PRINT N'El modo SQLCMD debe estar habilitado para ejecutar correctamente este script.';
+        SET NOEXEC ON;
+    END
+
+
+GO
+USE [$(DatabaseName)];
+
+
+GO
+
+
+
+MERGE INTO Enrollment AS Target
+USING (VALUES 
+(1, 2.00, 1, 1),
+(2, 3.50, 1, 2),
+(3, 4.00, 2, 3),
+(4, 1.80, 2, 1),
+(5, 3.20, 3, 1),
+(6, 4.00, 3, 2)
+)
+AS Source (EnrollmentID, Grade, CourseID, StudentID)
+ON Target.EnrollmentID = Source.EnrollmentID
+WHEN NOT MATCHED BY TARGET THEN
+INSERT (Grade, CourseID, StudentID)
+VALUES (Grade, CourseID, StudentID);
+GO
+
+GO
+PRINT N'Actualización completada.';
+
+
+GO
